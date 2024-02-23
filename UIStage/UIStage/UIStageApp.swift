882636866f030fe331aa0096d6277cdac690fa71
ezekiel102 <t.r.rustem7@gmail.com>
2024-02-23 22:35:35 +0300

    start uiproject

---
+++ UIStage/UIStage/UIStageApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIStageApp.swift
+//  UIStage
+//
+//  Created by Рустем on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct UIStageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
